04138ec7367d48e02a62d55b272ac4b91f8d4faf
Moritz Kork <moritz.kork@gmail.com>
2020-05-20 08:55:58 +0200

    rails day 2 - ex 2

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,21 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+
+  end
+
+  def edit
+      @task = Task.find(params[:id])
+  end
+
+  def create
+
+  end
+end
